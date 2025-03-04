b176e37652a1c0cd6074f4d9de0b7533b11abb3c
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-03-04 16:18:43 -0300

    fix domains.json file changes not being reflected in container

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ DEFAULT_RUN_TIME_SECONDS = "60"
 LOGGER_FORMAT = "%(asctime)s :: %(levelname)s :: %(message)s"
 
 # App data
-DOMAIN_FILE = "./domains.json"
+DOMAIN_FILE = "./config/domains.json"
 
 # URLs
 CF_BASE_URL = "https://api.cloudflare.com/client/v4"
@@ -18,3 +18,6 @@ IP_URLS = [
 # Regex patterns
 DOMAIN_PATTERN = r"@|[a-z0-9]([a-z0-9-].*[a-z0-9])?"
 IP_PATTERN = r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}"
+
+# Misc
+SECONDS_PER_HOUR = 3600
